cb41677b4d2af515300dd0eb6a058bf630007e42
jitendrasinghdevelopers <42056779+jitendrasinghdevelopers@users.noreply.github.com>
2024-11-19 19:49:58 +0530

    Update TaskTrackerConstants.java

--- src/main/java/com/tracker/tasktracker/constants/TaskTrackerConstants.java
+++ src/main/java/com/tracker/tasktracker/constants/TaskTrackerConstants.java
@@ -8,7 +8,7 @@ public class TaskTrackerConstants {
 	}
 	
 	public static final String CREATED_SUCCESSFULLY = "Task created successfully";
-    public static final String FETCH_SUCCESS = "Tasks fetched successfully";
+        public static final String FETCH_SUCCESS = "Tasks fetched successfully";
 	public static final String UPDATE_SUCCESS = "Task updated successfully";
 	public static final String DELETE_SUCCESS = "Task deleted successfully";
 	public static final String STATUS_UPDATED_SUCCESSFULLY = "Task status updated successfully";
